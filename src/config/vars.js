a3b2fb6090e99c19d1a50bd322e4e0e435b3400a
hamidrezanik00nia <hamidrezanik00nia@gmail.com>
2019-09-09 18:05:10 +0430

    env file

--- src/config/vars.js
+++ src/config/vars.js
@@ -3,7 +3,6 @@ const path = require('path');
 // import .env variables
 require('dotenv-safe').load({
   path: path.join(__dirname, '../../.env'),
-  sample: path.join(__dirname, '../../.env'),
 });
 
 module.exports = {
